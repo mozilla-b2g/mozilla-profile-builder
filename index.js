3289e3b2cc1e8f755d28f4ae9212fcd65f210e58
James Lal <james@lightsofapollo.com>
2013-07-17 14:52:22 -0700

    move to tmp

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// https://github.com/mozilla-b2g/mozilla-profile-builder/issues/2
-process.on('SIGINT', function() {});
-
 module.exports = {
   firefox: require('./lib/firefox'),
   b2g: require('./lib/b2g'),
